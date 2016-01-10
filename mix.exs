76ce46ecea96a1c447b2c051ad8057cc2c2b7cbd
Simone Carletti <weppos@weppos.net>
2016-01-10 17:20:10 +0100

    Start :httpoison at start

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Dnsimple.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :httpoison]]
   end
 
   # Dependencies can be Hex packages:

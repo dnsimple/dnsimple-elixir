9c878e0c4ea3518f81966a3324277d1fc5b2967e
Simone Carletti <weppos@weppos.net>
2016-01-27 19:27:05 +0100

    Add Domain fields

--- lib/dnsimple/domain.ex
+++ lib/dnsimple/domain.ex
@@ -1,4 +1,15 @@
 defmodule Dnsimple.Domain do
-  defstruct [:id, :name]
-  @type t :: %__MODULE__{id: integer, name: String.t}
+  defstruct [
+    :id, :account_id, :registrant_id,
+    :name, :token, :state,
+    :auto_renew, :private_whois,
+    :expired_on, :created_at, :updated_at,
+  ]
+  @type t :: %__MODULE__{
+    id: integer, account_id: integer, registrant_id: integer,
+    name: String.t, token: String.t, state: String.t,
+    auto_renew: boolean, private_whois: boolean,
+    expired_on: String.t, created_at: String.t, updated_at: String.t,
+  }
 end
+

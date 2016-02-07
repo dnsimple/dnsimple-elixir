a07bfd614b6667dd8b16584d17b339ab7c4b9a7b
Simone Carletti <weppos@weppos.net>
2016-02-08 00:00:12 +0100

    Document methods and module

--- lib/dnsimple/domains_service.ex
+++ lib/dnsimple/domains_service.ex
@@ -1,7 +1,20 @@
 defmodule Dnsimple.DomainsService do
+  @moduledoc """
+  DomainsService handles communication with the domain related
+  methods of the DNSimple API.
+
+  See https://developer.dnsimple.com/v2/domains/
+  """
+
   alias Dnsimple.Client
   alias Dnsimple.Domain
 
+
+  @doc """
+  List the domains.
+
+  See https://developer.dnsimple.com/v2/domains/#list
+  """
   @spec domains(Client.t, String.t | integer, Keyword.t) :: [Domain.t]
   def domains(client, account_id, options \\ []) do
     response = Client.get(client, Client.versioned("#{account_id}/domains"), options)
@@ -11,6 +24,11 @@ defmodule Dnsimple.DomainsService do
     |> Dnsimple.Utils.collection_to_struct(Domain)
   end
 
+  @doc """
+  Get a domain.
+
+  See https://developer.dnsimple.com/v2/domains/#get
+  """
   @spec domain(Client.t, String.t | integer, String.t | integer, Keyword.t) :: Domain.t
   def domain(client, account_id \\ Dnsimple.Client.__WILDCARD_ACCOUNT, domain_id, options \\ []) do
     response = Client.get(client, Client.versioned("#{account_id}/domains/#{domain_id}"), options)

d811ef758b7aba5c18714f7db7332d5e2eaf889d
Micah Wedemeyer <me@micahwedemeyer.com>
2016-02-08 22:50:44 -0500

    Add listZones

--- lib/dnsimple/zones_service.ex
+++ lib/dnsimple/zones_service.ex
@@ -2,6 +2,20 @@ defmodule Dnsimple.ZonesService do
   alias Dnsimple.Client
   alias Dnsimple.Zone
 
+  @spec zones(Client.t, Keyword.t) :: [Zone.t]
+  def zones(client, options \\ []) do
+    zones(client, "_", options)
+  end
+
+  @spec zones(Client.t, String.t | integer, Keyword.t) :: [Zone.t]
+  def zones(client, account_id, options) do
+    response = Client.get(client, Client.versioned("#{account_id}/zones"), options)
+    response.body
+    |> Poison.decode!
+    |> Map.get("data")
+    |> Dnsimple.Utils.collection_to_struct(Zone)
+  end
+
   @spec zone(Client.t, String.t | integer, Keyword.t) :: Zone.t
   def zone(client, zone_id, options \\ []) do
     zone(client, "_", zone_id, options)
--- test/dnsimple_zones_service_test.exs
+++ test/dnsimple_zones_service_test.exs
@@ -7,6 +7,30 @@ defmodule DnsimpleZonesServiceTest do
   @client %Dnsimple.Client{access_token: "i-am-a-token", base_url: "https://api.dnsimple.test/"}
 
 
+  test ".zones/2 builds the correct request" do
+    fixture = ExvcrUtils.response_fixture("listZones/success.http", [url: @client.base_url <> "v2/_/zones"])
+    use_cassette :stub, fixture do
+      @service.zones(@client)
+    end
+  end
+
+  test ".zones/3 builds the correct request" do
+    fixture = ExvcrUtils.response_fixture("listZones/success.http", [url: @client.base_url <> "v2/1010/zones"])
+    use_cassette :stub, fixture do
+      @service.zones(@client, 1010, [])
+    end
+  end
+
+  test ".zones returns a list of Dnsimple.Zone" do
+    use_cassette :stub, ExvcrUtils.response_fixture("listZones/success.http", [url: "~r/\/zones$/"]) do
+      response = @service.zones(@client, 1010, [])
+      assert is_list(response)
+      assert length(response) == 2
+      assert Enum.all?(response, fn(single) -> single.__struct__ == Dnsimple.Zone end)
+      assert Enum.all?(response, fn(single) -> is_integer(single.id) end)
+    end
+  end
+
   test ".zone/3 builds the correct request" do
     fixture = ExvcrUtils.response_fixture("getZone/success.http", [url: @client.base_url <> "v2/_/zones/example.weppos"])
     use_cassette :stub, fixture do

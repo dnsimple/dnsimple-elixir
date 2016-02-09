700d32c826d711485b484864a6d88fa26e22a90c
Micah Wedemeyer <me@micahwedemeyer.com>
2016-02-08 22:29:10 -0500

    Fix the specs for ZonesService

--- lib/dnsimple/zones_service.ex
+++ lib/dnsimple/zones_service.ex
@@ -2,12 +2,12 @@ defmodule Dnsimple.ZonesService do
   alias Dnsimple.Client
   alias Dnsimple.Zone
 
-  @spec zone(Client.t, String.t | integer, Keyword.t) :: [Zone.t]
+  @spec zone(Client.t, String.t | integer, Keyword.t) :: Zone.t
   def zone(client, zone_id, options \\ []) do
     zone(client, "_", zone_id, options)
   end
 
-  @spec zone(Client.t, String.t | integer, String.t | integer, Keyword.t) :: [Zone.t]
+  @spec zone(Client.t, String.t | integer, String.t | integer, Keyword.t) :: Zone.t
   def zone(client, account_id, zone_id, options) do
     response = Client.get(client, Client.versioned("#{account_id}/zones/#{zone_id}"), options)
     response.body
@@ -16,4 +16,3 @@ defmodule Dnsimple.ZonesService do
     |> Dnsimple.Utils.single_to_struct(Zone)
   end
 end
-

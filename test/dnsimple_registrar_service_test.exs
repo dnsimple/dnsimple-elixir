7fe57067215e575ae68ced6400911f74ba263d8f
Javier Acero <j4cegu@gmail.com>
2016-04-01 10:24:19 +0200

    Minor cleanup

--- test/dnsimple_registrar_service_test.exs
+++ test/dnsimple_registrar_service_test.exs
@@ -6,7 +6,11 @@ defmodule DnsimpleRegistrarServiceTest do
   @client %Dnsimple.Client{access_token: "i-am-a-token", base_url: "https://api.dnsimple.test"}
 
   test ".check_domain" do
-    use_cassette :stub, ExvcrUtils.response_fixture("checkDomain/success.http", method: "get", url: "#{@client.base_url}/v2/registrar/domains/example.com/check") do
+    fixture  = "checkDomain/success.http"
+    method   = "get"
+    endpoint = "#{@client.base_url}/v2/registrar/domains/example.com/check"
+
+    use_cassette :stub, ExvcrUtils.response_fixture(fixture, method: method, url: endpoint) do
       { :ok, response } = @service.check_domain(@client, "example.com")
 
       assert response.__struct__ == Dnsimple.Response

6a4a173c58bb87f3e5f477b5b5870b1ada90af17
Javier Acero <j4cegu@gmail.com>
2016-04-01 13:24:58 +0200

    Implement Registrar.register_domain

--- lib/dnsimple/registrar_service.ex
+++ lib/dnsimple/registrar_service.ex
@@ -1,12 +1,21 @@
 defmodule Dnsimple.RegistrarService do
   alias Dnsimple.Client
   alias Dnsimple.Response
+  alias Dnsimple.Domain
   alias Dnsimple.DomainCheck
 
   @spec check_domain(Client.t, String.t, Keyword.t) :: Response.t
   def check_domain(client, domain_name, options \\ []) do
     Client.get(client, Client.versioned("/registrar/domains/#{domain_name}/check"), options)
       |> Response.parse(DomainCheck)
+  end
+
+  @spec register_domain(Client.t, String.t, String.t, Keyword.t, Keyword.t, Keyword.t) :: Response.t
+  def register_domain(client, account_id, domain_name, attributes \\ [], headers \\ [], options \\ []) do
+    url = Client.versioned("/a/#{account_id}/registrar/domains/#{domain_name}/registration")
+
+    Client.post(client, url, attributes, headers, options)
+      |> Response.parse(Domain)
   end
 
 end
--- test/dnsimple_registrar_service_test.exs
+++ test/dnsimple_registrar_service_test.exs
@@ -6,16 +6,44 @@ defmodule DnsimpleRegistrarServiceTest do
   @client %Dnsimple.Client{access_token: "i-am-a-token", base_url: "https://api.dnsimple.test"}
 
   test ".check_domain" do
-    fixture  = "checkDomain/success.http"
-    method   = "get"
-    endpoint = "#{@client.base_url}/v2/registrar/domains/example.com/check"
+    fixture = "checkDomain/success.http"
+    method  = "get"
+    url     = "#{@client.base_url}/v2/registrar/domains/example.com/check"
 
-    use_cassette :stub, ExvcrUtils.response_fixture(fixture, method: method, url: endpoint) do
-      { :ok, response } = @service.check_domain(@client, "example.com")
+    use_cassette :stub, ExvcrUtils.response_fixture(fixture, method: method, url: url) do
+      {:ok, response} = @service.check_domain(@client, "example.com")
 
       assert response.__struct__ == Dnsimple.Response
       assert response.data.__struct__ == Dnsimple.DomainCheck
       assert response.data == %Dnsimple.DomainCheck{domain: "example.com", available: true, premium: false}
+    end
+  end
+
+  test ".register_domain" do
+    fixture     = "registerDomain/success.http"
+    method      = "post"
+    url         = "#{@client.base_url}/v2/a/1010/registrar/domains/example.com/registration"
+    attributes  = %{registrant_id: 2, auto_renew: false, privacy: false}
+    {:ok, body} = Poison.encode(attributes)
+
+    use_cassette :stub, ExvcrUtils.response_fixture(fixture, method: method, url: url, request_body: body) do
+      {:ok, response} = @service.register_domain(@client, "1010", "example.com", attributes)
+
+      assert response.__struct__ == Dnsimple.Response
+      assert response.data.__struct__ == Dnsimple.Domain
+      assert response.data == %Dnsimple.Domain{
+        id: 1,
+        name: "example.com",
+        account_id: 1010,
+        registrant_id: 2,
+        auto_renew: false,
+        private_whois: false,
+        state: "registered",
+        token: "cc8h1jP8bDLw5rXycL16k8BivcGiT6K9",
+        created_at: "2016-01-16T16:08:50.649Z",
+        updated_at: "2016-01-16T16:09:01.161Z",
+        expired_on: nil,
+      }
     end
   end
 

d7b108ef95747c77f9a8da646b13a3ec6a9ba5d8
Javier Acero <j4cegu@gmail.com>
2016-04-06 15:28:32 +0200

    Add documentation

--- lib/dnsimple/registrar_service.ex
+++ lib/dnsimple/registrar_service.ex
@@ -4,6 +4,11 @@ defmodule Dnsimple.RegistrarService do
   alias Dnsimple.Domain
   alias Dnsimple.DomainCheck
 
+  @doc """
+  Checks whether a domain is available to be registered.
+
+  See: https://developer.dnsimple.com/v2/registrar/#check
+  """
   @spec check_domain(Client.t, String.t, String.t, Keyword.t, Keyword.t) :: Response.t
   def check_domain(client, account_id, domain_name, headers \\ [], options \\ []) do
     url = Client.versioned("/#{account_id}/registrar/domains/#{domain_name}/check")
@@ -12,6 +17,11 @@ defmodule Dnsimple.RegistrarService do
       |> Response.parse(DomainCheck)
   end
 
+  @doc """
+  Registers a domain.
+
+  See https://developer.dnsimple.com/v2/registrar/#check
+  """
   @spec register_domain(Client.t, String.t, String.t, Keyword.t, Keyword.t, Keyword.t) :: Response.t
   def register_domain(client, account_id, domain_name, attributes \\ [], headers \\ [], options \\ []) do
     url = Client.versioned("/#{account_id}/registrar/domains/#{domain_name}/registration")
@@ -20,6 +30,11 @@ defmodule Dnsimple.RegistrarService do
       |> Response.parse(Domain)
   end
 
+  @doc """
+  Renews a domain.
+
+  See https://developer.dnsimple.com/v2/registrar/#renew
+  """
   @spec renew_domain(Client.t, String.t, String.t, Keyword.t, Keyword.t, Keyword.t) :: Response.t
   def renew_domain(client, account_id, domain_name, attributes \\ [], headers \\ [], options \\ []) do
     url = Client.versioned("/#{account_id}/registrar/domains/#{domain_name}/renew")
@@ -28,6 +43,11 @@ defmodule Dnsimple.RegistrarService do
       |> Response.parse(Domain)
   end
 
+  @doc """
+  Starts the transfer of a domain to DNSimple.
+
+  See https://developer.dnsimple.com/v2/registrar/#transfer
+  """
   @spec transfer_domain(Client.t, String.t, String.t, Keyword.t, Keyword.t, Keyword.t) :: Response.t
   def transfer_domain(client, account_id, domain_name, attributes \\ [], headers \\ [], options \\ []) do
     url = Client.versioned("/#{account_id}/registrar/domains/#{domain_name}/transfer")
@@ -36,6 +56,11 @@ defmodule Dnsimple.RegistrarService do
       |> Response.parse(Domain)
   end
 
+  @doc """
+  Requests the transfer of a domain out of DNSimple.
+
+  See https://developer.dnsimple.com/v2/registrar/#transfer_out
+  """
   @spec transfer_domain_out(Client.t, String.t, String.t, Keyword.t, Keyword.t) :: Response.t
   def transfer_domain_out(client, account_id, domain_name, headers \\ [], options \\ []) do
     url = Client.versioned("/#{account_id}/registrar/domains/#{domain_name}/transfer_out")

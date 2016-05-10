59bd60ea531ef5e209fc714150c691cbefa2765b
Simone Carletti <weppos@weppos.net>
2016-05-10 14:59:48 +0200

    Release 0.1.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Dnsimple.Mixfile do
 
   def project do
     [app: :dnsimple,
-     version: "0.0.1",
+     version: "0.1.0",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,

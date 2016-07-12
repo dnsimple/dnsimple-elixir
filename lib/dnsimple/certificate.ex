aaa5ff173c792d1f6398230f8a2030c65c506673
Anthony Eden <me@anthonyeden.com>
2016-07-12 11:29:13 +0200

    Feature/domain certificates endpoint (#26)
    * Add Dnsimple.Certificate struct
    * Implement domain : certificates list operation
    * V2 domain : certificate get certificate
    * Add API v2 domain : certificate download and get private key
    * Use `Integer.t` type.

---
+++ lib/dnsimple/certificate.ex
@@ -0,0 +1,28 @@
+defmodule Dnsimple.Certificate do
+  @moduledoc """
+  Represents a [DNSimple certificate](https://developer.dnsimple.com/v2/certificates/#certificate-attributes)
+  """
+
+  @fields [
+    :id, :domain_id,
+    :name, :common_name,
+    :years, :csr, :state,
+    :authority_identifier,
+    :server, :root, :chain, # Only from download endpoint
+    :private_key,           # Only from private key endpoint
+    :expires_on, :created_at, :updated_at,
+  ]
+
+  @type t :: %__MODULE__{
+    id: Integer.t, domain_id: Integer.t,
+    name: String.t, common_name: String.t,
+    years: Integer.t, csr: String.t, state: String.t,
+    authority_identifier: String.t,
+    server: String.t, root: String.t, chain: String.t,
+    private_key: String.t,
+    expires_on: String.t, created_at: String.t, updated_at: String.t
+  }
+
+  defstruct @fields
+
+end
---
+++ lib/dnsimple/domain_certificates_service.ex
@@ -0,0 +1,61 @@
+defmodule Dnsimple.DomainCertificatesService do
+  @moduledoc """
+  DomainCertificatesService handles communication with the DNSimple
+  API responsible for providing certificates attached to domains.
+
+  @see https://developer.dnsimple.com/v2/domains/certificates/
+  """
+
+  alias Dnsimple.Client
+  alias Dnsimple.ListOptions
+  alias Dnsimple.Response
+  alias Dnsimple.Certificate
+
+  @doc """
+  List certificates.
+
+  See https://developer.dnsimple.com/v2/domains/certificates/#list
+  """
+  @spec certificates(Client.t, String.t | integer, String.t | integer) :: Response.t
+  def certificates(client, account_id, domain_id, options \\ []) do
+    {headers, opts} = Client.headers(options)
+    Client.get(client, Client.versioned("/#{account_id}/domains/#{domain_id}/certificates"), headers, ListOptions.prepare(opts))
+    |> Response.parse(Certificate)
+  end
+
+  @doc """
+  Get a certificate.
+
+  See https://developer.dnsimple.com/v2/domains/certificates/#get
+  """
+  @spec certificate(Client.t, String.t | integer, String.t | integer, String.t | integer, Keyword.t) :: Response.t
+  def certificate(client, account_id, domain_id, certificate_id, options \\ []) do
+    {headers, opts} = Client.headers(options)
+    Client.get(client, Client.versioned("/#{account_id}/domains/#{domain_id}/certificates/#{certificate_id}"), headers, opts)
+    |> Response.parse(Certificate)
+  end
+
+  @doc """
+  Download a certificate.
+
+  See https://developer.dnsimple.com/v2/domains/certificates/#download
+  """
+  @spec download(Client.t, String.t | integer, String.t | integer, String.t | integer, Keyword.t) :: Response.t
+  def download(client, account_id, domain_id, certificate_id, options \\ []) do
+    {headers, opts} = Client.headers(options)
+    Client.get(client, Client.versioned("/#{account_id}/domains/#{domain_id}/certificates/#{certificate_id}/download"), headers, opts)
+    |> Response.parse(Certificate)
+  end
+
+  @doc """
+  Download a private key.
+
+  See https://developer.dnsimple.com/v2/domains/certificates/#get-private-key
+  """
+  @spec private_key(Client.t, String.t | integer, String.t | integer, String.t | integer, Keyword.t) :: Response.t
+  def private_key(client, account_id, domain_id, certificate_id, options \\ []) do
+    {headers, opts} = Client.headers(options)
+    Client.get(client, Client.versioned("/#{account_id}/domains/#{domain_id}/certificates/#{certificate_id}/private_key"), headers, opts)
+    |> Response.parse(Certificate)
+  end
+end

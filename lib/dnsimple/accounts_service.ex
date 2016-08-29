f002ba4ec256fec4d4882f049fe8a42b60d296a7
Javier Acero <j4cegu@gmail.com>
2016-08-29 10:30:09 +0200

    Add support for listing accounts (#29)
    * Copy fixtures from developer repo
    
    * Add support for list accounts endpoint
    
    * Include the plan identifier
    
    * Test using Elixir ~> 1.3

---
+++ lib/dnsimple/accounts_service.ex
@@ -0,0 +1,27 @@
+defmodule Dnsimple.AccountsService do
+  @moduledoc """
+  AccountsService handles communication with the account related
+  methods of the DNSimple API.
+
+  See https://developer.dnsimple.com/v2/accounts/
+  """
+
+  alias Dnsimple.Client
+  alias Dnsimple.Account
+  alias Dnsimple.Response
+  alias Dnsimple.ListOptions
+
+
+  @doc """
+  Lists the accounts
+
+  See https://developer.dnsimple.com/v2/accounts/#list
+  """
+  @spec accounts(Client.t) :: Response.t
+  def accounts(client, options \\ []) do
+    {headers, opts} = Client.headers(options)
+    Client.get(client, Client.versioned("/accounts"), headers, ListOptions.prepare(opts))
+    |> Response.parse(Account)
+  end
+
+end

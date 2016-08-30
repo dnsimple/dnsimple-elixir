a7342ebc74a792551b0da36cbee8f64d93a15a79
Javier Acero <j4cegu@gmail.com>
2016-08-30 16:01:56 +0200

    Update auto-renewal related function APIs
    To be the following:
    - Dnsimple.Registrar.enable_auto_renewal
    - Dnsimple.Registrar.disable_auto_renewal
    
    These functions will be under the same module as the existing registrar
    related ones so we expose the same API as in other clients.

--- lib/dnsimple/registrar.ex
+++ lib/dnsimple/registrar.ex
@@ -77,6 +77,35 @@ defmodule Dnsimple.Registrar do
 
 
   @doc """
+  Enable auto-renewal for the domain.
+
+  See: https://developer.dnsimple.com/v2/registrar/auto-renewal/#enable
+  """
+  @spec enable_auto_renewal(Client.t, String.t, String.t, Keyword.t) :: Response.t
+  def enable_auto_renewal(client, account_id, domain_name, options \\ []) do
+    url = Client.versioned("/#{account_id}/registrar/domains/#{domain_name}/auto_renewal")
+    {headers, opts} = Client.headers(options)
+
+    Client.put(client, url, headers, opts)
+      |> Response.parse(nil)
+  end
+
+  @doc """
+  Disable auto-renewal for the domain.
+
+  See: https://developer.dnsimple.com/v2/registrar/auto-renewal/#disable
+  """
+  @spec disable_auto_renewal(Client.t, String.t, String.t, Keyword.t) :: Response.t
+  def disable_auto_renewal(client, account_id, domain_name, options \\ []) do
+    url = Client.versioned("/#{account_id}/registrar/domains/#{domain_name}/auto_renewal")
+    {headers, opts} = Client.headers(options)
+
+    Client.delete(client, url, headers, opts)
+      |> Response.parse(nil)
+  end
+
+
+  @doc """
   Gets the whois privacy for the domain.
 
   See: https://developer.dnsimple.com/v2/registrar/whois-privacy/#get
--- test/dnsimple/registrar_test.exs
+++ test/dnsimple/registrar_test.exs
@@ -119,6 +119,35 @@ defmodule Dnsimple.RegistrarTest do
   end
 
 
+  test ".enable_auto_renewal" do
+    fixture = "enableDomainAutoRenewal/success.http"
+    method  = "put"
+    url     = "#{@client.base_url}/v2/1010/registrar/domains/example.com/auto_renewal"
+    attributes  = []
+    {:ok, body} = Poison.encode(attributes)
+
+    use_cassette :stub, ExvcrUtils.response_fixture(fixture, method: method, url: url, request_body: body) do
+      {:ok, response} = @service.enable_auto_renewal(@client, "1010", "example.com")
+
+      assert response.__struct__ == Dnsimple.Response
+      assert is_nil(response.data)
+    end
+  end
+
+  test ".disable_auto_renewal" do
+    fixture = "disableDomainAutoRenewal/success.http"
+    method  = "delete"
+    url     = "#{@client.base_url}/v2/1010/registrar/domains/example.com/auto_renewal"
+
+    use_cassette :stub, ExvcrUtils.response_fixture(fixture, method: method, url: url) do
+      {:ok, response} = @service.disable_auto_renewal(@client, "1010", "example.com")
+
+      assert response.__struct__ == Dnsimple.Response
+      assert is_nil(response.data)
+    end
+  end
+
+
   test ".whois_privacy" do
     fixture = "getWhoisPrivacy/success.http"
     method  = "get"

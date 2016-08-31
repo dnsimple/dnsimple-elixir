61555183a8f04294d9169bb12194083922de288d
Javier Acero <j4cegu@gmail.com>
2016-08-31 16:38:17 +0200

    Use new methods for services

--- lib/dnsimple/services.ex
+++ lib/dnsimple/services.ex
@@ -7,7 +7,6 @@ defmodule Dnsimple.Services do
   """
 
   alias Dnsimple.Client
-  alias Dnsimple.ListOptions
   alias Dnsimple.Response
   alias Dnsimple.Service
 
@@ -18,8 +17,9 @@ defmodule Dnsimple.Services do
   """
   @spec applied_services(Client.t, String.t | integer, String.t | integer) :: Response.t
   def applied_services(client, account_id, domain_id, options \\ []) do
-    {headers, opts} = Client.headers(options)
-    Client.get(client, Client.versioned("/#{account_id}/domains/#{domain_id}/services"), headers, ListOptions.prepare(opts))
+    url = Client.versioned("/#{account_id}/domains/#{domain_id}/services")
+
+    Client.get_list(client, url, options)
     |> Response.parse(Service)
   end
 
@@ -30,8 +30,9 @@ defmodule Dnsimple.Services do
   """
   @spec apply_service(Client.t, String.t | integer, String.t | integer, String.t | integer) :: Response.t
   def apply_service(client, account_id, domain_id, service_id, options \\ []) do
-    {headers, opts} = Client.headers(options)
-    Client.post(client, Client.versioned("/#{account_id}/domains/#{domain_id}/services/#{service_id}"), nil, headers, opts)
+    url = Client.versioned("/#{account_id}/domains/#{domain_id}/services/#{service_id}")
+
+    Client.post2(client, url, _attributes = nil, options)
     |> Response.parse(nil)
   end
 
@@ -42,8 +43,10 @@ defmodule Dnsimple.Services do
   """
   @spec unapply_service(Client.t, String.t | integer, String.t | integer, String.t | integer) :: Response.t
   def unapply_service(client, account_id, domain_id, service_id, options \\ []) do
-    {headers, opts} = Client.headers(options)
-    Client.delete(client, Client.versioned("/#{account_id}/domains/#{domain_id}/services/#{service_id}"), headers, opts)
+    url = Client.versioned("/#{account_id}/domains/#{domain_id}/services/#{service_id}")
+
+    Client.delete2(client, url, options)
     |> Response.parse(nil)
   end
+
 end

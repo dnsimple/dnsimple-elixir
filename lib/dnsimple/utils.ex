65a9e0adfbb5ac44457eb58188a92b850951e8d0
Javier Acero <j4cegu@gmail.com>
2016-09-08 11:09:40 +0200

    Refactor attrs_to_struc
    The idea is to be able to provide nested struct types. This will be
    required to handle TldExtendedAttribute.Option parsing.

--- lib/dnsimple/utils.ex
+++ lib/dnsimple/utils.ex
@@ -1,38 +1,64 @@
 defmodule Dnsimple.Utils do
 
   @doc """
-  Converts the map into a struct.
+  Converts a ap into a struct.
 
   This function is similar to `Kernel.struct/2`, however it handles
   the case where the struct fields are defined with atoms, and the map
-  instead is passed with keys as strings.
+  uses strings as keys.
 
-  Moreover, compared to `struct/2` the order of the parameter is swapped
-  so that it's possible to use the `|>` operator pipelining the map.
+  It also handles the conversion of nested attribute maps into structs.
+  This can only be done at the top level.
+
+  This is correct and will work:
+
+      attrs = %{"a" => "b", "c" => %{"d" => "e"}}
+      attrs_to_struct(attrs, MyStruct, c: MyNestedStruct)
+
+  Where this is not correct and will not be converted correctly:
+
+      attrs = %{"a" => "b", "c" => %{"d" => %{"e" => "f"}}}
+      attrs_to_struct(attrs, MyStruct, c: MyNestedStruct, e: OtherNestedSTruct)
+
+  Lists of maps are also converted correctly both at the top and the nested level.
+  So you can do things like:
+
+      attrs = %{"a" => "b", "c" => [%{"d" => "e"}, %{"d" => "f"}]}
+      attrs_to_struct(attrs, MyStruct, c: MyNestedStruct)
+
+  On top of this the order of the first two parameters has been swapped
+  to be able to use the method in pipelines.
 
   ## Examples
 
-      defmodule User, do: defstruct [:id, :name]
-      {:module, User, _, %User{id: nil, name: nil}}
+      defmodule Address, do: defstruct [:street, :country]
+      defmodule User, do: defstruct [:id, :name, :address]
 
       Dnsimple.Utils.attrs_to_struct(%{"name" => "weppos", "foo" => "bar"}, User)
-      %User{id: nil, name: "weppos"}
+      Dnsimple.Utils.attrs_to_struct([%{"name" => "jacegu"}, %{"name" => "weppos"}], User)
+      Dnsimple.Utils.attrs_to_struct(%{"id" => 1, "address" => %{"country" => "IT"}}, User, address: Address)
 
   """
-  @spec attrs_to_struct(Enum.t, module | map) :: map
-  def attrs_to_struct(attrs, kind) do
-    struct = struct(kind)
-    Enum.reduce Map.to_list(struct), struct, fn {k, _}, acc ->
-      case Map.fetch(attrs, Atom.to_string(k)) do
-        {:ok, v} -> %{acc | k => v}
-        :error -> acc
-      end
-    end
+  @spec attrs_to_struct(Enum.t, module | map, Keyword.t) :: map
+  def attrs_to_struct(attrs, kind, nested \\ [])
+  def attrs_to_struct(attrs, kind, nested) when is_list(attrs) do
+    Enum.map(attrs, &(attrs_to_struct(&1, kind, nested)))
+  end
+  def attrs_to_struct(attrs, kind, nested) do
+    attributes = Enum.reduce(attrs, %{}, fn({name, value}, result) ->
+      name  = String.to_atom(name)
+      value = value(name, value, nested)
+      Map.put(result, name, value)
+    end)
+
+    struct(kind, attributes)
   end
 
-  @spec attrs_to_structs(List.t, module | map) :: map
-  def attrs_to_structs(collection, kind) do
-    Enum.map(collection, fn(x) -> attrs_to_struct(x, kind) end)
+  defp value(key, value, nested) do
+    case Keyword.get(nested, key) do
+      nil         -> value
+      nested_kind -> attrs_to_struct(value, nested_kind)
+    end
   end
 
 

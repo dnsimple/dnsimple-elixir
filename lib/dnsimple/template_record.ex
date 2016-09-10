80405f816416f80b8d1df27d7f761693c34e6d1c
Javier Acero <j4cegu@gmail.com>
2016-09-10 11:49:14 +0200

    Add struct representing a template record

---
+++ lib/dnsimple/template_record.ex
@@ -0,0 +1,22 @@
+defmodule Dnsimple.TemplateRecord do
+  @moduledoc """
+  Represents a template record.
+
+  See: https://developer.dnsimple.com/v2/templates/records/
+  """
+
+  @type t :: %__MODULE__{
+    id: integer,
+    template_id: integer,
+    type: String.t,
+    name: String.t,
+    content: String.t,
+    ttl: integer,
+    priority: integer,
+    created_at: String.t,
+    updated_at: String.t,
+  }
+
+  defstruct ~w(id template_id type name content ttl priority created_at updated_at)a
+
+end

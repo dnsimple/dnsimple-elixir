7a00e8e81c9ba83351cca86640aff03c5c9718ec
Javier Acero <j4cegu@gmail.com>
2016-09-13 11:10:15 +0200

    Revert "Example of decoders with Poison"
    This reverts commit a1dd60581692ceb100f5c4cd56a9652df9b38af4.

--- test/dnsimple/tlds_test.exs
+++ test/dnsimple/tlds_test.exs
@@ -5,28 +5,6 @@ defmodule Dnsimple.TldsTest do
   @module Dnsimple.Tlds
   @client %Dnsimple.Client{access_token: "i-am-a-token", base_url: "https://api.dnsimple.test"}
 
-  test "poison test" do
-    body = """
-{"data":[{"name":"uk_legal_type","description":"Legal type of registrant contact","required":false,"options":[{"title":"UK Individual","value":"IND","description":"UK Individual (our default value)"},{"title":"Non-UK Individual (representing self)","value":"FIND","description":"Non-UK Individual (representing self)"},{"title":"UK Limited Company","value":"LTD","description":"UK Limited Company"},{"title":"UK Public Limited Company","value":"PLC","description":"UK Public Limited Company"},{"title":"UK Partnership","value":"PTNR","description":"UK Partnership"},{"title":"UK LLP","value":"LLP","description":"UK Limited Liability Partnership"},{"title":"UK Sole Trader","value":"STRA","description":"UK Sole Trader"},{"title":"UK Registered Charity","value":"RCHAR","description":"UK Registered Charity"},{"title":"UK Industrial/Provident Registered Company","value":"IP","description":"UK Industrial/Provident Registered Company"},{"title":"UK School","value":"SCH","description":"UK School"},{"title":"Other Foreign","value":"FOTHER","description":"Other foreign organizations"},{"title":"UK Government Body","value":"GOV","description":"UK Government Body"},{"title":"UK Corporation by Royal Charter","value":"CRC","description":"UK Corporation by Royal Charter"},{"title":"UK Statutory Body","value":"STAT","description":"UK Statutory Body"},{"title":"UK Entity (other)","value":"OTHER","description":"UK Entity (other)"},{"title":"Non-UK Corporation","value":"FCORP","description":"Non-UK Corporation"},{"title":"Non-UK Organization (other)","value":"FOTHER","description":"Non-UK Organization"}]},{"name":"uk_reg_co_no","description":"Company identification number","required":false,"options":[]},{"name":"registered_for","description":"Company or person domain is registered for (this must be accurate and CANNOT BE CHANGED)","required":true,"options":[]},{"name":"uk_reg_opt_out","description":"Hide registrant data in Nominet WHOIS","required":false,"options":[{"title":"No","value":"n","description":"Do not hide the registrant contact information in Nominet's WHOIS."},{"title":"Yes","value":"y","description":"Hide the registrant contact information in Nominet's WHOIS (only available to individuals)."}]}]}
-    """
-    {:ok, result} = Poison.decode(body, as: %{"data" => [%Dnsimple.TldExtendedAttribute{options: [%Dnsimple.TldExtendedAttribute.Option{}]}]})
-
-    data = Map.get(result, "data")
-
-    [first_attribute | _] = data
-    assert first_attribute.__struct__ == Dnsimple.TldExtendedAttribute
-    assert first_attribute.name == "uk_legal_type"
-    assert first_attribute.description == "Legal type of registrant contact"
-    assert first_attribute.required == false
-    assert is_list(first_attribute.options)
-
-    [first_option | _] = first_attribute.options
-    assert first_option.__struct__ == Dnsimple.TldExtendedAttribute.Option
-    assert first_option.title == "UK Individual"
-    assert first_option.value == "IND"
-    assert first_option.description == "UK Individual (our default value)"
-  end
-
   describe ".list_tlds" do
     setup do
       url = "#{@client.base_url}/v2/tlds"

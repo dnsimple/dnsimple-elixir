9733af81504785ac75f4335f680340c5cd846f7f
Javier Acero <j4cegu@gmail.com>
2016-09-14 12:15:55 +0200

    Struct definition impromevents
    Define Service struct following the same conventions.

--- lib/dnsimple/service.ex
+++ lib/dnsimple/service.ex
@@ -1,21 +1,19 @@
 defmodule Dnsimple.Service do
   @moduledoc """
-  Represents a one-click [DNSimple service](https://developer.dnsimple.com/v2/services/#service-attributes).
-  """
+  Represents a one-click service.
 
-  @fields [
-    :id,
-    :name, :short_name,
-    :description,
-    :created_at, :updated_at
-  ]
+  See https://developer.dnsimple.com/v2/services/#service-attributes.
+  """
 
   @type t :: %__MODULE__{
     id: integer,
-    name: String.t, short_name: String.t,
+    name: String.t,
+    short_name: String.t,
     description: String.t,
-    created_at: String.t, updated_at: String.t
+    created_at: String.t,
+    updated_at: String.t,
   }
 
-  defstruct @fields
+  defstruct ~w(id name short_name description created_at updated_at)a
+
 end

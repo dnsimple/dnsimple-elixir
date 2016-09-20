3aa640522a4e84e572f39612282977d471e373dd
Javier Acero <j4cegu@gmail.com>
2016-09-20 12:36:08 +0200

    Add regression test

--- test/dnsimple/identity_test.exs
+++ test/dnsimple/identity_test.exs
@@ -6,15 +6,44 @@ defmodule Dnsimple.IdentityTest do
   @client %Dnsimple.Client{access_token: "i-am-a-token", base_url: "https://api.dnsimple.test"}
 
 
-  test ".whoami" do
-    use_cassette :stub, ExvcrUtils.response_fixture("whoami/success.http", [url: "~r/\/v2\/whoami/$"]) do
-      {:ok, response} = @module.whoami(@client)
-      assert response.__struct__ == Dnsimple.Response
+  describe ".whoami" do
+    setup do
+      {:ok, method: "get", url: "#{@client.base_url}/v2/whoami"}
+    end
 
-      data = response.data
-      assert is_map(data)
-      assert data.__struct__ == Dnsimple.Whoami
-      assert %Dnsimple.Whoami{account: _, user: _} = data
+    test "returns the user in a DNSimple response", %{method: method, url: url} do
+      use_cassette :stub, ExvcrUtils.response_fixture("whoami/success_user.http", method: method, url: url) do
+        {:ok, response} = @module.whoami(@client)
+        assert response.__struct__ == Dnsimple.Response
+
+        data = response.data
+        assert data.__struct__ == Dnsimple.Whoami
+
+        assert data.account == nil
+
+        user = data.user
+        assert user.__struct__ == Dnsimple.User
+        assert user.id == 1
+        assert user.email == "example-user@example.com"
+      end
+    end
+
+    test "returns the account in a DNSimple response", %{method: method, url: url} do
+      use_cassette :stub, ExvcrUtils.response_fixture("whoami/success_account.http", method: method, url: url) do
+        {:ok, response} = @module.whoami(@client)
+        assert response.__struct__ == Dnsimple.Response
+
+        data = response.data
+        assert data.__struct__ == Dnsimple.Whoami
+
+        assert data.user == nil
+
+        account = data.account
+        assert account.__struct__ == Dnsimple.Account
+        assert account.id == 1
+        assert account.email == "example-account@example.com"
+        assert account.plan_identifier == nil
+      end
     end
   end
 

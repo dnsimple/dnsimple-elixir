cb63ecb2428084f54d4b352bff1ce02fbad25d5e
Javier Acero <j4cegu@gmail.com>
2016-09-20 12:40:15 +0200

    Correctly decode the whoami response payload

--- lib/dnsimple/identity.ex
+++ lib/dnsimple/identity.ex
@@ -2,18 +2,27 @@ defmodule Dnsimple.Identity do
   alias Dnsimple.Client
   alias Dnsimple.Response
   alias Dnsimple.Whoami
+  alias Dnsimple.Account
+  alias Dnsimple.User
 
   @doc """
   Returns information about the currently authenticated user and/or account.
 
   See: https://developer.dnsimple.com/v2/identity/#whoami
+
+  ## Examples:
+
+    client = %Dnsimple.Client{access_token: "a1b2c3d4"}
+
+    Dnsimple.Identity.whoami(client)
+
   """
   @spec whoami(Client.t, Keyword.t) :: Response.t
   def whoami(client, options \\ []) do
     url = Client.versioned("/whoami")
 
     Client.get(client, url, options)
-    |> Response.parse(%{"data" => %Whoami{}})
+    |> Response.parse(%{"data" => %Whoami{account: %Account{}, user: %User{}}})
   end
 
 end

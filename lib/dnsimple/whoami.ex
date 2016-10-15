aea6d9f66dcfe353ddff72048877917cc003b356
Javier Acero <j4cegu@gmail.com>
2016-10-15 16:17:51 +0200

    Cleanup Whoami struct definition

--- lib/dnsimple/whoami.ex
+++ lib/dnsimple/whoami.ex
@@ -1,4 +1,15 @@
 defmodule Dnsimple.Whoami do
-  defstruct [:user, :account]
-  @type t :: %__MODULE__{user: Dnsimple.User.t, account: Dnsimple.Account.t}
+  @moduledoc """
+  Represents the credentials used to login to the API.
+
+  See https://developer.dnsimple.com/v2/identity/
+  """
+
+  @type t :: %__MODULE__{
+    user: Dnsimple.User.t,
+    account: Dnsimple.Account.t
+  }
+
+  defstruct ~w(user account)a
+
 end

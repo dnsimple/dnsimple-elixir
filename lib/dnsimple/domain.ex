b250dc5420cc78ac12bf1ffd622cdaf383dc2a9a
Javier Acero <j4cegu@gmail.com>
2016-10-15 16:03:09 +0200

    Cleanup domain struct definition.

--- lib/dnsimple/domain.ex
+++ lib/dnsimple/domain.ex
@@ -1,22 +1,26 @@
 defmodule Dnsimple.Domain do
   @moduledoc """
-  Represents a [DNSimple domain](https://developer.dnsimple.com/v2/domains/#domain-attributes).
+  Represents a domain.
+
+  See https://developer.dnsimple.com/v2/domains/
+  See https://developer.dnsimple.com/v2/domains/#domain-attributes
   """
 
-  @fields [
-    :id, :account_id, :registrant_id,
-    :name, :token, :state,
-    :auto_renew, :private_whois,
-    :expires_on, :created_at, :updated_at,
-  ]
-
   @type t :: %__MODULE__{
-    id: integer, account_id: integer, registrant_id: integer,
-    name: String.t, token: String.t, state: String.t,
-    auto_renew: boolean, private_whois: boolean,
-    expires_on: String.t, created_at: String.t, updated_at: String.t,
+    id: integer,
+    account_id: integer,
+    registrant_id: integer,
+    name: String.t,
+    token: String.t,
+    state: String.t,
+    auto_renew: boolean,
+    private_whois: boolean,
+    expires_on: String.t,
+    created_at: String.t,
+    updated_at: String.t,
   }
 
-  defstruct @fields
+  defstruct ~w(id account_id registrant_id name token state auto_renew
+               private_whois expires_on created_at updated_at)a
 
 end

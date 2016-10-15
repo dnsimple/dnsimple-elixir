a67dc775cb58e90b6a2d40fedb7564cbee004ca0
Javier Acero <j4cegu@gmail.com>
2016-10-15 16:15:49 +0200

    Cleanup DomainCheck struct definition

--- lib/dnsimple/domain_check.ex
+++ lib/dnsimple/domain_check.ex
@@ -1,4 +1,16 @@
 defmodule Dnsimple.DomainCheck do
-  defstruct [:domain, :available, :premium]
-  @type t :: %__MODULE__{domain: String.t, available: boolean, premium: boolean}
+  @moduledoc """
+  Represent a domain check.
+
+  See https://developer.dnsimple.com/v2/registrar/#check
+  """
+
+  @type t :: %__MODULE__{
+    domain: String.t,
+    available: boolean,
+    premium: boolean,
+  }
+
+  defstruct ~w(domain available premium)a
+
 end

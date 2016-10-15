fb0c7da136e38167b5e2228b5d69654541cc5619
Javier Acero <j4cegu@gmail.com>
2016-10-15 16:16:02 +0200

    Cleanup OauthToken struct definition

--- lib/dnsimple/oauth_token.ex
+++ lib/dnsimple/oauth_token.ex
@@ -1,4 +1,18 @@
 defmodule Dnsimple.OauthToken do
-  defstruct [:access_token, :token_type, :scope, :account_id]
-  @type t :: %__MODULE__{access_token: String.t, token_type: String.t, scope: String.t, account_id: integer}
+  @moduledoc """
+  Represents an OAuth token.
+
+  See https://developer.dnsimple.com/v2/oauth/
+  See https://developer.dnsimple.com/v2/oauth/#step-2---access-token
+  """
+
+  @type t :: %__MODULE__{
+    access_token: String.t,
+    token_type: String.t,
+    scope: String.t,
+    account_id: integer,
+  }
+
+  defstruct ~w(access_token token_type scope account_id)a
+
 end

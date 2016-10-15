79166fc1e18c3b4c318d24ccd156393be9b5ccb3
Javier Acero <j4cegu@gmail.com>
2016-10-15 16:07:50 +0200

    Cleanup certificate struct definition

--- lib/dnsimple/certificate.ex
+++ lib/dnsimple/certificate.ex
@@ -1,28 +1,36 @@
 defmodule Dnsimple.Certificate do
   @moduledoc """
-  Represents a [DNSimple certificate](https://developer.dnsimple.com/v2/certificates/#certificate-attributes)
+  Represents a DNSimple certificate.
+
+  The `server`, `root` and `chain` attributes are only relevant for the
+  download endpoint accessible through `Dnsimple.Certificates.download_certificate`.
+
+  The `private_key` attribute is only relevant for the private key endpoint
+  accessible through `Dnsimple.Certificates.get_certificate_private_key`.
+
+  See https://developer.dnsimple.com/v2/certificates
+  See https://developer.dnsimple.com/v2/certificates/#certificate-attributes
   """
 
-  @fields [
-    :id, :domain_id,
-    :name, :common_name,
-    :years, :csr, :state,
-    :authority_identifier,
-    :server, :root, :chain, # Only from download endpoint
-    :private_key,           # Only from private key endpoint
-    :expires_on, :created_at, :updated_at,
-  ]
-
   @type t :: %__MODULE__{
-    id: integer, domain_id: integer,
-    name: String.t, common_name: String.t,
-    years: integer, csr: String.t, state: String.t,
+    id: integer,
+    domain_id: integer,
+    name: String.t,
+    common_name: String.t,
+    years: integer,
+    csr: String.t,
+    state: String.t,
     authority_identifier: String.t,
-    server: String.t, root: String.t, chain: String.t,
+    server: String.t,
+    root: String.t,
+    chain: String.t,
     private_key: String.t,
-    expires_on: String.t, created_at: String.t, updated_at: String.t
+    expires_on: String.t,
+    created_at: String.t,
+    updated_at: String.t,
   }
 
-  defstruct @fields
+  defstruct ~w(id domain_id name common_name years csr state authority_identifier
+               server root chain private_key expires_on created_at updated_at)a
 
 end

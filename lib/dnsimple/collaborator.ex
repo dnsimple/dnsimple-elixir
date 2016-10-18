8e8d6a572bd51d480e83a6f76dd9858266d65c53
Javier Acero <j4cegu@gmail.com>
2016-10-18 10:28:21 +0200

    Add Collaborator struct

---
+++ lib/dnsimple/collaborator.ex
@@ -0,0 +1,23 @@
+defmodule Dnsimple.Collaborator do
+  @moduledoc """
+  Represents a collaborator in a domain.
+
+  See https://developer.dnsimple.com/v2/domains/collaborators
+  """
+
+  @type t :: %__MODULE__{
+    id: integer,
+    domain_id: integer,
+    domain_name: String.t,
+    user_id: integer,
+    user_email: String.t,
+    invitation: boolean,
+    accepted_at: String.t,
+    created_at: String.t,
+    updated_at: String.t,
+  }
+
+  defstruct ~w(id domain_id domain_name user_id user_email invitation
+               accepted_at created_at updated_at)a
+
+end

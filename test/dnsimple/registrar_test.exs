98b768295c5ab1bdb82355a5233a5afe4ff0f88d
Cadu Ribeiro <mail@cadu.dev>
2021-04-13 11:51:41 -0300

    Fix spec

--- test/dnsimple/registrar_test.exs
+++ test/dnsimple/registrar_test.exs
@@ -61,7 +61,7 @@ defmodule Dnsimple.RegistrarTest do
   describe ".get_domain_prices" do
     test "returns the result in a Dnsimple.Response for the domain" do
       url = "#{@client.base_url}/v2/#{@account_id}/registrar/domains/bingo.pizza/prices"
-      fixture = "getdomainPrices/success.http"
+      fixture = "getDomainPrices/success.http"
 
       use_cassette :stub, ExvcrUtils.response_fixture(fixture, method: "get", url: url) do
         {:ok, response} = @module.get_domain_prices(@client, @account_id, "bingo.pizza")

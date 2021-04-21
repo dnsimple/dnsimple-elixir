93d5ef2ee35665f16dfb5a2c3643890f98f452d7
Ole Michaelis <Ole.Michaelis@googlemail.com>
2021-04-21 10:06:14 +0200

    Release 3.0.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Dnsimple.Mixfile do
 
   def project do
     [app: :dnsimple,
-     version: "2.0.0",
+     version: "3.0.0",
      elixir: "~> 1.6",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,

fc9318613cc06e3f09eb983e8953fe32ac0c63fc
Juan Ku Quintana <juankuquintana@gmail.com>
2026-03-23 12:37:51 -0600

    Release v8.2.0

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Dnsimple.Mixfile do
   use Mix.Project
 
   @source_url "https://github.com/dnsimple/dnsimple-elixir"
-  @version "8.1.0"
+  @version "8.2.0"
 
   def project do
     [app: :dnsimple,

723aff90b6b0b9cde76e51978417897f1ff839e8
Simone Carletti <simone.carletti@dnsimple.com>
2026-09-18 09:48:08 +0200

    Add registrar domain restore (#356)
    This PR adds `restore_domain` and `get_domain_restore` to the registrar service. `restore_domain` restores a domain that can no longer be renewed, and `get_domain_restore` gets the details of a domain restore.

---
+++ lib/dnsimple/domain_restore.ex
@@ -0,0 +1,19 @@
+defmodule Dnsimple.DomainRestore do
+  @moduledoc """
+  Represents a domain restore.
+
+  See:
+  - https://developer.dnsimple.com/v2/registrar/#restoreDomain
+  """
+  @moduledoc section: :data_types
+
+  @type t :: %__MODULE__{
+          id: integer,
+          domain_id: integer,
+          state: String.t(),
+          created_at: String.t(),
+          updated_at: String.t()
+        }
+
+  defstruct ~w(id domain_id state created_at updated_at)a
+end
